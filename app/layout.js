0cad62d1e056d4951da5c48e07748d89435a67a3
Daxt61 <bene.gesserit1@gmail.com>
2026-02-21 16:29:52 +0100

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Guitar Fingering Optimizer',
+  description: 'Optimise tes doigtés de guitare',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  )
+}
